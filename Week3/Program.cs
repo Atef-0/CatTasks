97ceeb01b5c1c48d44d8ca3e55742a51cf0f0cdd
Atef-0 <mohammedatefezzat@gmail.com>
2025-03-23 00:19:12 +0200

    h

--- Week3/Program.cs
+++ Week3/Program.cs
@@ -1,8 +1,5 @@
 ﻿internal class Program
 {
-   // 1-Create a generic method Swap<T> that swaps two variables of any type.
-   //    *Test the method with integers and strings.
-   //    * Output the results before and after the swap.'
 
     public static void Swap<T>(ref T x, ref T y)
     {
@@ -34,11 +31,33 @@
         Dictionary<string, int> dic = new Dictionary<string, int> { { "h", 12 }, { "d", 45 }, { "g", 87 },  { "k", 65 },{ "x", 88 }, };
         FindStudent("h", dic);
         FindStudent("aaaa", dic);
- 
- 
- 
+        Console.WriteLine("------------------------------------------------");
+        Queue<string> q = new Queue<string>();
+        q.Enqueue("mohammed");
+        q.Enqueue("ahmed");
+        q.Enqueue("Iyad");
+        q.Enqueue("Mori");
+        q.Enqueue("Mills");
+        q.Enqueue("Zuko");
+        Console.WriteLine($"Currently serving: {q.Dequeue()}");
+        Console.WriteLine($"Next customer in line: {q.Peek()}");
+        Console.WriteLine($"First Customer done\nnow serving: {q.Dequeue()}");
+        Console.WriteLine("remaining customers: ");
+        foreach (var item in q)
+        {
+            Console.WriteLine(item);
+        }
+
+
+
+
+
 
 
     }
     
 }
+//3-Implement a Queue<string> to simulate a customer service line.
+//*Add customers to the queue.
+//*Process them one by one, displaying their names as they are served.
+
